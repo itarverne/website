399ad38650e94e8ba6ef18c52026a6b1c407f218
general03 <drigaudie@yahoo.fr>
2019-11-08 14:28:14 +0100

    Revert "Add images"
    This reverts commit 1dfd1d2cd1ae6aa3acef293386721c83027a4ea2.

---
+++ scripts/react/main-react.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import FacebookProvider, { Like, Share as ShareFb} from 'react-facebook';
+import ReactLinkedIn from 'react-share-linkedin';
+
+class SocialLinkedinShare extends React.Component {
+  render() {
+    return (
+      <ReactLinkedIn 
+        url="http://itarverne.com"
+        title="The web developer expert in Auvergne"
+        summary="The web developer expert in Auvergne"
+        borderRadius="3px"
+      />
+    );
+  }
+}
+ReactDOM.render(
+  <SocialLinkedinShare />,
+  document.getElementById('js-in-share')
+);
+
+const ButtonFb = styled.button`
+  border-radius: 3px;
+  font-size: 11px;
+  height: 20px;
+  padding: 0 5px 0 5px;
+  background-color: rgb(66, 103, 178);
+  color: white;
+  font-weight: bold;
+
+  &:hover {
+    background: #365899;
+    cursor: pointer;
+  }
+`;
+
+class SocialFbLike extends React.Component {
+  render() {
+    return (
+      <FacebookProvider appId="474812546226673">
+        <Like href="https://www.facebook.com/itarverne/" colorScheme="dark" layout="button_count" showFaces />
+      </FacebookProvider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <SocialFbLike />,
+  document.getElementById('js-fb-like')
+);
+
+class SocialFbShare extends React.Component {
+  render() {
+    return (
+      <FacebookProvider appId="474812546226673">
+        <ShareFb href="http://www.itarverne.com">
+          <ButtonFb>Share</ButtonFb>
+        </ShareFb>
+      </FacebookProvider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <SocialFbShare />,
+  document.getElementById('js-fb-share')
+);
+
+import { Share } from 'react-twitter-widgets'
+ 
+ReactDOM.render((
+  <Share
+    url="itarverne.com"
+    options={{
+      text: '@itarverne est une société auvergnate spécialisée dans le développement web Python et Java',
+      lang: 'fr',
+      count: 'horizontal'
+    }}
+    onLoad={() => console.log('Share twitter is loaded!')}
+  />
+), document.getElementById('js-twitter-share'));
+
+import { Follow } from 'react-twitter-widgets'
+ 
+ReactDOM.render((
+  <Follow
+    username="itarverne"
+    onLoad={() => console.log('Follow twitter is loaded!')}
+  />
+), document.getElementById('js-twitter-follow'));
